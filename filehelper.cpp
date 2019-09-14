01c9014926cf53fba7577550942a5d32fafec42b
Schachigel <holger@mairon.net>
2019-09-14 21:38:06 +0200

    refactor backup creation

--- filehelper.cpp
+++ filehelper.cpp
@@ -9,7 +9,7 @@
 
 bool overwrite_copy(const QString& from, const QString& to)
 {
-    qDebug() << from << " to " << to << endl;
+    qDebug() << "overwrite_copy " << from << " to " << to;
     if( QFile().exists(to))
         QFile().remove(to);
     return QFile().copy(from, to);
@@ -17,16 +17,15 @@ bool overwrite_copy(const QString& from, const QString& to)
 
 bool backupFile(const QString&  fn)
 {
+    // generate list of backup filenames
     QList<QString> backupnames;
     for(int i = 0; i<10; i++)
     {
-        QString nbr;
-        QTextStream ts(&nbr); ts.setFieldWidth(2); ts.setPadChar('0');
-        ts << i;
-        QString backupextension (QString("-"+ nbr + ".bak"));
-        QString name = fn + backupextension;
+        QString nbr(QString ("%1").arg(i,int(2),10, QLatin1Char('0')));
+        QString name = fn + QString("-"+ nbr + ".bak");
         backupnames.append(name);
     }
+    // copy existing files to filename with next index from top to bottom
     bool ret(true);
     if( QFile().exists(backupnames[9]))
         ret &= QFile().remove(backupnames[9]);
@@ -37,15 +36,15 @@ bool backupFile(const QString&  fn)
         if( !overwrite_copy(backupnames[i], backupnames[i+1]))
         {
             ret = false;
-            qDebug() << "Backup copy failed. File to be copied: " << backupnames[i] << endl;
+            qDebug() << "Backup copy failed. File to be copied: " << backupnames[i];
         }
     }
-
+    // copy the last file: the one w the filename of the file you want to create
     if( QFile().exists(fn))
         if( !overwrite_copy(fn, backupnames[0]))
         {
             ret = false;
-            qDebug() << "Backup copy failed. File to be copied: " << backupnames[0] << endl;
+            qDebug() << "Backup copy failed. File to be copied: " << backupnames[0];
         }
     return ret;
 }
