26b999b25a4add4c0e94a813015e56ff69b30ccf
Schachigel <holger@mairon.net>
2020-06-12 23:18:15 +0200

    wip: termination wizard

---
+++ DKV2/wizterminatecontract.cpp
@@ -0,0 +1,6 @@
+#include "wizterminatecontract.h"
+
+wizTerminateContract::wizTerminateContract()
+{
+
+}
---
+++ DKV2/wizterminatecontract.h
@@ -0,0 +1,11 @@
+#ifndef WIZTERMINATECONTRACT_H
+#define WIZTERMINATECONTRACT_H
+
+
+class wizTerminateContract
+{
+public:
+    wizTerminateContract();
+};
+
+#endif // WIZTERMINATECONTRACT_H
