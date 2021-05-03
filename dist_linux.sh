fc391749002f07190f6e66d7d13f44d256c3c93c
Franz Heinzmann (Frando) <frando@unbiskant.org>
2021-05-03 14:14:11 +0200

    add github action to build for linux
    see https://github.com/Schachigel/DKV2/pull/11 for details

---
+++ dist_linux.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# Stop at any error
+set -e
+
+# Location of the source tree
+SOURCEDIR=`pwd`/DKV2
+PROJECTFILE=DKV2.pro
+# Location to build DKV2
+BUILDDIR=`pwd`/build-dist-linux
+
+# Prepare QT build enviornment: accept either QTDIR or Qt5_Dir env variable
+# for QT location
+if [ -z ${QTDIR+x} ]; then
+	QTDIR=${Qt5_Dir}
+fi
+if [ -z ${QTDIR+x} ]; then
+	echo "QTDIR not defined- please set it to the location containing the Qt version to build against. For example:"
+	echo "  export QTDIR=~/Qt/5.15.0/gcc_64"
+	exit 1
+fi
+QMAKE=${QTDIR}/bin/qmake
+MAKE=make
+# LINUXDEPLOYQT="linuxdeployqt"
+LINUXDEPLOYQT=`pwd`/linuxdeployqt-continuous-x86_64.AppImage
+GIT_VERSION=`git rev-parse --short HEAD`
+VERSION=`git describe --tags 2> /dev/null || echo $GIT_VERSION`
+
+##### build #####
+
+mkdir -p ${BUILDDIR}
+pushd ${BUILDDIR}
+
+${QMAKE} ${SOURCEDIR}/${PROJECTFILE} \
+    -spec linux-g++ \
+    CONFIG+=qtquickcompiler
+
+${MAKE} -j6
+
+##### package with linuxdeployqt #####
+
+mkdir -p app
+pushd app
+cp ${SOURCEDIR}/res/logo256.png dkv2.png
+cp ${SOURCEDIR}/res/DKV2.desktop ./
+mv ${BUILDDIR}/DKV2 ./
+popd
+
+unset LD_LIBRARY_PATH # Remove too old Qt from the search path
+# LINUXDEPLOYQT_OPTS=-unsupported-allow-new-glibc
+PATH=${QTDIR}/bin:${PATH} ${LINUXDEPLOYQT} app/DKV2 -bundle-non-qt-libs ${LINUXDEPLOYQT_OPTS}
+PATH=${QTDIR}/bin:${PATH} ${LINUXDEPLOYQT} app/DKV2 -appimage ${LINUXDEPLOYQT_OPTS}
+
+APPIMAGE_GIT_FILENAME="DKV2-${GIT_VERSION}-x86_64.AppImage"
+APPIMAGE_RES_FILENAME="DKV2-${VERSION}-x86_64.AppImage"
+ARTIFCACT_FILENAME="DKV2-${VERSION}-x86_64.tar.gz"
+
+mv ${APPIMAGE_GIT_FILENAME} ${APPIMAGE_RES_FILENAME} 2>/dev/null | true
+tar -czf ${ARTIFCACT_FILENAME} ${APPIMAGE_RES_FILENAME}
+echo "Created ${BUILDDIR}/${ARTIFCACT_FILENAME}"
