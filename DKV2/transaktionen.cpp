b0be6eb9c4be34f3bebc96b207b7a215dbc21bd6
Schachigel <holger@mairon.net>
2021-11-29 20:38:26 +0100

    correct typo

--- DKV2/transaktionen.cpp
+++ DKV2/transaktionen.cpp
@@ -94,7 +94,7 @@ void newCreditorAndContract()
     }
 
     if( not wiz.field(pnConfirmContract).toBool()) {
-        qInfo() << "user decided not to save the creditor";
+        qInfo() << "user decided not to save the contract";
         return;
     }
     contract cont;
