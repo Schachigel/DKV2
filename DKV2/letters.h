1f6350847ab0720647b60bcf0139cf0d551bd7b5
Wilhelm Pflueger <Wilhelm.Pflueger@mailbox.org>
2021-12-02 19:06:40 +0100

    Add missing files letters.cpp/.h

---
+++ DKV2/letters.h
@@ -0,0 +1,28 @@
+#ifndef LETTERS_H
+#define LETTERS_H
+
+#include "lettersnippets.h"
+
+enum class letterType
+{
+    all = snippet::allLetters,
+    annPayoutL = int(interestModel::payout) + 1,
+    annReinvestL = int(interestModel::reinvest) + 1,
+    annInterestInfoL = int(interestModel::fixed) + 1,
+    annInfoL = int(interestModel::zero) + 1,
+    maxValue
+};
+int fromLetterType(letterType lt);
+
+letterType letterTypeFromInt(int lt);
+
+class letters
+{
+public:
+    letters();
+};
+
+/// init for new db
+int writeDefaultSnippets(QSqlDatabase db = QSqlDatabase::database());
+
+#endif // LETTERS_H
