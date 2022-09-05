5b1d1aac211c3c74d332b2c290f277058a66dfcb
Holger Mairon <holger@mairon.net>
2022-09-05 21:10:24 +0200

    again: fix compatibility with old qt versions

--- DKV2/contracttablemodel.cpp
+++ DKV2/contracttablemodel.cpp
@@ -13,7 +13,12 @@ QDate dateFromNP(const QString& s)
      * (x Monate) /n dd.mm.yyy
      */
 
+#if QT_VERSION_CHECK(5, 14, 0)
     QStringList lines =s.split(qsl("\xA"), Qt::SkipEmptyParts);
+#else
+    QStringList lines =s.split(qsl("\xA"), QString::SkipEmptyParts);
+#endif
+
     QString dateStr;
     if( 0 == lines.count()) {
         qWarning() << "empty contract termination string";
@@ -70,7 +75,7 @@ QVariant ContractTableModel::data(const QModelIndex& index, int role) const
 
 bool ContractProxyModel::lessThan(const QModelIndex &left, const QModelIndex &right) const
 {
-    
+
     if (left.column() == cp_ContractEnd) {
         int leftDtce = sourceModel()->data(left, Qt::UserRole).toInt();
         int rightDtce = sourceModel()->data(right, Qt::UserRole).toInt();
